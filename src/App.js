6cd0d541a06fdebcec7616de91f646bc054601f6
LeeSeongWon05 <lsw050103@gmail.com>
2023-09-25 10:41:56 +0900

    second_save

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 function App() {
-	return (
-		<h1>Hello</h1>
-	);
+	return <h1>Hello</h1>;
 }
 
 export default App;
+
+/*
+	SSR vs CSR
+
+	SSR - Server Side Rendering
+	* 페이지 이동시마다 일일이 서버쪽에 HTML파일을 요청해서 가져오는 방식
+	* 장점 : 초기로딩속도가 빠름 , 검색엔진 최적화 ( SEO 좋음 )
+
+	CSR - Client Side Rendering
+
+*/
